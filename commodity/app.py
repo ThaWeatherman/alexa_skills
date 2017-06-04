682076c95cc455e1ae42f8580a1d14d20b1cd20d
Sean Beck <seanmckaybeck@gmail.com>
2017-06-04 19:20:19 +0000

    updated commodity skill
    now supports querying for available commodities
    now catches exception for None

--- commodity/app.py
+++ commodity/app.py
@@ -11,9 +11,9 @@ ask = Ask(app, '/')
 
 @ask.intent('CommodityPrice', mapping={'commodity': 'Commodity'})
 def price(commodity):
-    commodity = commodity.lower()
-    url = 'http://spot.seanmckaybeck.com/api/' + commodity
     try:
+        commodity = commodity.lower()
+        url = 'http://spot.seanmckaybeck.com/api/' + commodity
         r = requests.get(url)
         if 'error' in r.json():
             speech_output = "Unknown commodity {}.".format(commodity)
@@ -26,6 +26,24 @@ def price(commodity):
             speech_output = "{} the price of {} is ${}.".format(date_str, commodity, r.json()[commodity])
     except requests.exceptions.HTTPError:
         speech_output = "I'm sorry, there was an error retrieving the price. Try again later."
+    except AttributeError:
+        # happens when commodity is None
+        speech_output = "I'm sorry, I didn't receive a commodity in your request."
+    return statement(speech_output).simple_card('Commodity', speech_output)
+
+
+@ask.intent('AvailableCommodities')
+def available():
+    url = 'http://spot.seanmckaybeck.com/api/all'
+    try:
+        r = requests.get(url)
+        speech_output = 'The following are supported commodities: '
+        d = r.json()
+        del d['last']
+        a = ', '.join(d.keys())
+        speech_output += a
+    except requests.exceptions.HTTPError:
+        speech_output = "I'm sorry, there was an error retrieving the available commodities. Try again later."
     return statement(speech_output).simple_card('Commodity', speech_output)
 
 
