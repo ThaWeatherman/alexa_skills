f0ad1ec4865bbdf56fe8f347dd6c933f0b165ab0
Sean Beck <seanmckaybeck@gmail.com>
2017-06-06 02:04:06 +0000

    updated to add help, cancel, and stop intents

--- commodity/app.py
+++ commodity/app.py
@@ -7,11 +7,14 @@ import requests
 
 app = Flask(__name__)
 ask = Ask(app, '/')
+with open('speech_assets/LIST_OF_COMMODITIES') as f:
+    app.config['SUPPORTED'] = [ c.strip() for c in f.readlines() ]
+HELP = "You can ask for the available commodities or for the price of a specific commodity. What can I do for you?"
 
 
 @ask.launch
 def launched():
-    return question('Welcome to Commodity Prices')
+    return question('Welcome to Commodity Prices. ' + HELP)
 
 
 @ask.session_ended
@@ -26,7 +29,9 @@ def price(commodity):
         url = 'http://spot.seanmckaybeck.com/api/' + commodity
         r = requests.get(url)
         if 'error' in r.json():
-            speech_output = "Unknown commodity {}.".format(commodity)
+            speech_output = "I'm sorry but I don't track the commodity {}. ".format(commodity)
+            speech_output += "You can ask about any of the following: " + ", ".join(app.config['SUPPORTED'])
+            return question(speech_output)
         else:
             d = datetime.datetime.strptime(r.json()['last'], '%Y-%m-%d %H:%M:%S.%f')
             if d.date() == datetime.datetime.today():
@@ -35,26 +40,34 @@ def price(commodity):
                 date_str = 'As of {} on {}, '.format(d.time().strftime('%I:%M %p'), d.date().strftime('%B %d'))
             speech_output = "{} the price of {} is ${}.".format(date_str, commodity, r.json()[commodity])
     except requests.exceptions.HTTPError:
-        speech_output = "I'm sorry, there was an error retrieving the price. Try again later."
+        speech_output = "I'm sorry, there was an error retrieving the price. Please try again later."
     except AttributeError:
         # happens when commodity is None
-        speech_output = "I'm sorry, I didn't receive a commodity in your request."
-    return statement(speech_output).simple_card('Commodity', speech_output)
+        speech_output = "I'm sorry, I didn't receive a valid commodity in your request. "
+        speech_output += "You can ask about any of the following: " + ", ".join(app.config['SUPPORTED'])
+        return question(speech_output)
+    return statement(speech_output).simple_card('Commodity Prices', speech_output)
 
 
 @ask.intent('AvailableCommodities')
 def available():
-    url = 'http://spot.seanmckaybeck.com/api/all'
-    try:
-        r = requests.get(url)
-        speech_output = 'The following are supported commodities: '
-        d = r.json()
-        del d['last']
-        a = ', '.join(d.keys())
-        speech_output += a
-    except requests.exceptions.HTTPError:
-        speech_output = "I'm sorry, there was an error retrieving the available commodities. Try again later."
-    return statement(speech_output).simple_card('Commodity', speech_output)
+    speech_output = "The following are supported commodities: " + ", ".join(app.config['SUPPORTED'])
+    return statement(speech_output).simple_card('Commodity Prices', speech_output)
+
+
+@ask.intent('AMAZON.HelpIntent')
+def help():
+    return question(HELP).reprompt("I didn't get that, what can I do for you?")
+
+
+@ask.intent('AMAZON.StopIntent')
+def stop():
+    return statement('Goodbye')
+
+
+@ask.intent('AMAZON.CancelIntent')
+def cancel():
+    return statement('Goodbye')
 
 
 if __name__ == '__main__':
